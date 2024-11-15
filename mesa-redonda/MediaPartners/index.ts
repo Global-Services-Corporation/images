3e8c2785a23e207cbe3bf24bdca6b56bc1751a36
joelsilva333 <joelpitra44@gmail.com>
2024-11-15 17:31:42 +0100

    mesa redonda pics added

---
+++ mesa-redonda/MediaPartners/index.ts
@@ -0,0 +1,9 @@
+import checkin360 from "./checkin360.png"
+import otelegrama from "./otelegrama.png"
+import logoPlatinaLine from "./logoPlatinaLine.png"
+
+export {
+    logoPlatinaLine,
+    otelegrama,
+    checkin360
+}
---
+++ mesa-redonda/aboutEvent/index.ts
@@ -0,0 +1,7 @@
+import participar from "./Vector-1.png"
+import expor from "./Vector-2.png"
+import patrocinar from "./Vector-3.png"
+import speakers from "./Vector-4.png"
+import MassanoPic from "./MassanoImage.png"
+
+export { participar, expor, patrocinar, speakers, MassanoPic }
